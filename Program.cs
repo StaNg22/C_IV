1f5780ecef38eaf68d1542b9abef17118172c818
StaNg22 <21211tt3190@mail.tdc.edu.vn>
2021-11-07 21:14:58 +0700

    first commt

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lap_Trinh_Cua_Toi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("Hello World");
+        }
+    }
+}

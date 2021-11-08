a5668c033c7b6e7e4593b5bcdb048e8e5e9a09bb
StaNg22 <21211tt3190@mail.tdc.edu.vn>
2021-11-08 18:22:26 +0700

    BT

---
+++ CHUONG_IV/Class_14.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+14.Viết chương trình trò chơi One-Two-Three ra cái gì ra cái này theo điều kiện:
+	Búa(B) thắng Kéo, thua Giấy.
+	Kéo (K) thắng Giấy, thua Búa.
+	Giấy (G) thắng Búa, thua Kéo.
+	Hướng dẫn: Dùng lệnh switch lồng nhau.
+ */
+namespace CHUONG_I
+{
+    class Class_14
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("---One - Two - Three---");
+
+        }
+    }
+}

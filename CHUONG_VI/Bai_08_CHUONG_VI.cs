6db419a3269eb547172533893c7f098c19cbf0a4
StaNg22 <21211tt3190@mail.tdc.edu.vn>
2021-11-16 07:57:59 +0700

    BT

---
+++ CHUONG_VI/Bai_08_CHUONG_VI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+8.Viết hàm tính số hạng thứ n của dãy Fibonaci.
+	Dãy Fibonaci là dãy số gồm các số hạng F(n) với:
+F0 = F1 = 1; Fn = Fn - 1 + Fn - 2
+	Dãy Fibonaci sẽ là: 1 1 2 3 5 8 13 21 34 55 89 144…
+Trong hàm main, nhập vào số nguyên không âm n, in ra số hạng thứ n của dãy Fibo ra màn hình.
+ */
+namespace CHUONG_VI
+{
+    internal class Bai_08_CHUONG_VI
+    {
+        static void TF(double n)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //khai báo biến
+            double F0 = 1;//giá trị thứ hạng F0
+            double F1 = 1;//giá trị thứ hạng F1
+            double Fn = 0;//giá trị thứ hàng Fn
+
+            //input
+            
+            if (n < 0)
+            {
+                Console.WriteLine("Vui lòng nhập lại!!!");
+            }
+            else
+            {
+                if (n <= 2)
+                {
+                    Fn = n;
+                    Console.WriteLine($"Giá trị F{n} = {Fn}");
+                }
+                else
+                {
+                    for (int i = 2; i <= n; i++)
+                    {
+                        Fn = F0 + F1;
+                        F0 = F1;
+                        F1 = Fn;
+                    }
+                    Console.WriteLine($"Giá trị F{n} = {Fn}");
+                }
+            }
+        }
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double n = 0;
+            Console.WriteLine("Nhập vào số nguyên n(n>0):");
+            n = Convert.ToDouble(Console.ReadLine());
+            if (n > 0)
+            {
+                TF(n);
+            }
+            else
+            {
+                Console.WriteLine("Vui lòng nhập lại!!!");
+            }
+        }
+    }
+}
---
+++ CHUONG_VI/Bai_10_CHUONG_VI.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//10.Viết chương trình đếm các số hoàn hảo nhỏ hơn hoặc bằng N, 
+//   với N là số nguyên dương được nhập vào từ bàn phím.
+namespace CHUONG_VI
+{
+    internal class Bai_10_CHUONG_VI
+    {
+        static bool dshh(double n)
+        {
+            int dem = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                {
+                    dem += i;
+                }
+            }
+            if (dem == n)
+            {
+                return true;
+            }
+            return false;
+        }
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            double n = 0;
+            int dem = 0;
+            Console.WriteLine("Nhập vào số nguyên dương n:");
+            n = Convert.ToDouble(Console.ReadLine());
+            
+            if (n <= 0)
+            {
+                Console.WriteLine($"{n} không thỏa yêu cầu !!!");
+            }
+            else
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (dshh(i) == true)
+                    {
+                        dem++;
+                    }
+                }
+                Console.WriteLine($"Số số hoàn hảo từ 1 đến {n} là : {dem}");
+            }
+        }
+    }
+}

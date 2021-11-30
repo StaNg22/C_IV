96b7b3c9486b3861214e0db591befa575a4d8e1c
StaNg22 <93861577+StaNg22@users.noreply.github.com>
2021-11-30 10:14:39 +0700

    Update Bai_01_CHUONG_V.cs

--- CHUONG_V/Bai_01_CHUONG_V.cs
+++ CHUONG_V/Bai_01_CHUONG_V.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,9 @@ C = 1 + 1/2 + 1/3 + . . . + 1/n-1.
 D = 2 * 4 * 6 * … 2n.
 E = N! = 1 * 2 * . .  * n.
  */
+/*Name:Nguyenn_Le_Nhut_Quyen
+ *Date:19/11/2021
+ */
 namespace CHUONG_V
 {
     class Bai_01_CHUONG_V
@@ -18,48 +21,60 @@ namespace CHUONG_V
         static void Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            Console.WriteLine("Nhập vào số nguyên n:");
-            int n =Convert.ToInt32.Par(Console.ReadLine());
-            int A = 0, B = 1, E = 1, D = 1; 
+            
+            //Khai Báo Biến
+            int n = 0;
+            int A = 0;
+            int B = 1;
+            int E = 1;
+            int D = 1; 
             float C = 0;
+            string chon;
+
+            //input
+            Console.WriteLine("Nhập vào số nguyên n:");
+            n =Convert.ToInt32(Console.ReadLine());
+            
             Console.WriteLine("Bạn chọn cái gì(A, B, C, D):");
-            string chọn = Console.ReadLine();
+            chon = Console.ReadLine();
+            int kq;
+            //tính
             for (int i = 1; i <= n; i++)
             {
                 if (i % 2 != 0)
                 {
                     A += i;
                 }
-                B *= i * 3;
-                C += 1f / i;
                 E *= i;
+                D *= (2 * i);
             }
-            for (int i = 1; i < 2*n; i++)
+            for (int i = 1; i < n; i++)
             {
-                if (i % 2 == 0)
-                {
-                    D *= i;
-                }
+                C += 1f / i;
             }
-            switch (chọn)
+            for (int i = 1; 3*i <= n; i++)
+            {
+                kq=(n==0)?B=n:B *= i*3;
+            }
+            switch (chon)
             {
                 case "A":
                     Console.WriteLine($"Tổng các số lẻ bé hơn {n} là: {A}");
                     break;
                 case "B":
-                    Console.WriteLine($"Tích các bội của 3 nhỏ hơn {n} là: {B}");
+                    Console.WriteLine($"Tích các bội của 3 nhỏ hơn bằng {n} là: {B}");
                     break;
                 case "C":
-                    Console.WriteLine($" 1 + 1/2 + 1/3 + . . . + 1/{n-1} = {C}");
+                    Console.WriteLine($" 1 +. . . + 1/{n-1} = {C}");
                     break;
                 case "D":
-                    Console.WriteLine($"2 * 4 * 6 * … {2*n} = {D}");
+                    Console.WriteLine($"2 * … * {2*n} = {D}");
                     break;
                 case "E":
                     Console.WriteLine($"{n}! = {E}");
                     break;
                 default:
-                    Console.WriteLine($"{chọn} không thuộc A, B, C, D. VUI LONG NHẬP LẠI!!!");
+                    Console.WriteLine($"{chon} không thuộc A, B, C, D. VUI LONG NHẬP LẠI!!!");
                     break;
             }
         }

923ed41f93ba5fbbf00d5a2c9c389f03a3b67c73
StaNg22 <93861577+StaNg22@users.noreply.github.com>
2021-12-23 21:37:17 +0700

    Update Bai_01_CHUONG_VII.cs

--- CHUONG_VII/Bai_01_CHUONG_VII.cs
+++ CHUONG_VII/Bai_01_CHUONG_VII.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,105 +26,134 @@ namespace CHUONG_VII
 {
     internal class Bai_01_CHUONG_VII
     {
-        //Hàm A
-        static void nMang(int[] a,int n)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write($"A[{i}] = ");
-                a[i] = int.Parse(Console.ReadLine());
-            }
-        } 
-        //Hàm B
-        static void XMang(int[] b,int n)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                Console.Write("  {0}  ",b[i]);
-            }
-        }
-        //Hàm C
-        static void iSoChan(int[] d, int n)
-        {
-            for (int i = 0; i < n; i++)
-            {
-                if (d[i] % 2 == 0)
-                {
-                    Console.Write($"{d[i]}  ");
-                }
-            }
-        }
-
-        //Hàm D
-        static void iSoNT(int[] d,int n)
-        {
-            int dem = 0;
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 1; j <= d[i]; j++)
-                {
-                    if (d[i] % j == 0)
-                    {
-                        dem++;
-                    }
-                }
-                if (dem == 2)
-                {
-                    Console.Write($"{d[i]}  ");
-                    dem = 0;
-                }
-                dem = 0;
-            }
-        }
-
-        //Hàm E
-        static void tTB(int[] e,int n)
-        {
-            double tBinh = 0;
-            double demTB = 0;
-            for (int i = 0; i < n; i++)
-            {
-                demTB += e[i];
-            }
-            tBinh = demTB / n;
-            Console.WriteLine($"{tBinh}");
-        }
-
-        //Hàm F
-        static void kTraSHoanThien(int[] f, int n)
-        {
-            int dem = 0,dem1=0;
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 1; j < f[i]; j++)
-                {
-                    if (f[i] % j == 0)
-                    {
-                        dem += j;
-                    }
-                }
-                if (dem == f[i])
-                {
-                    dem1++;
-                    dem = 0;
-                }
-                dem = 0;
-            }
-            Console.WriteLine($"{dem1}");
-        }
-
-        //Hàm G
-        static void tVTCuoiCung(int[] g,int n)
-        {
+        
+        static void Main(string[] args)
+        { 
             Console.OutputEncoding = Encoding.UTF8;
+            int n = 0;
+            int[] arrA;
+            int[] arrB;
+            n = NhapSoLuongPhanTu();
+            arrA = NhapMang(n);
+            Console.WriteLine("- Mảng arrA vừa nhập là:");
+            XuatMang(arrA);
+            XuatSoChan(arrA);
+            XuatSoNguyenTo(arrA);
+            Console.Write("- Trung bình mảng là: ");
+            Console.WriteLine(TrungBinhCong(arrA));
+            DemSoHoanThien(arrA);
+            TimVTCuoiCung(arrA);
+            ViTriSoNTDauTien(arrA);
+            TimPhanLonNhat(arrA);
+            TimPhanNhoNhat(arrA);
+            arrB = SaoChepMang(arrA, n);
+            SapXepMang(arrB);
+            Console.WriteLine("- Mảng arrA có được sắp xếp tăng dần hay không?");
+            Console.WriteLine(ktrsx(arrA));
+        }
+        static bool ktrsx(int[] arr)
+        {
+            bool ktr = true;
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    ktr = false;
+                }
+            }
+            return ktr;
+        }
+        static void SapXepMang(int[] arr)
+        {
+            Console.WriteLine("- Mảng arrA được sắp xếp lại là:");
+            int i;
+            int j;
+            for (i = 0; i < arr.Length - 1; i++)
+            {
+                for (j = arr.Length - 1; j > i; j--)
+                {
+                    if (arr[j] < arr[j - 1])
+                    {
+                        int tmp = arr[j];
+                        arr[j] = arr[j - 1];
+                        arr[j - 1] = tmp;
+                    }
+                }
+            }
+            XuatMang(arr);
+        } 
+    
+        static int[] SaoChepMang(int[] arr, int n)
+        {
+            int[] arrB = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arrB[i] = arr[i];
+            }
+            return arrB;
+        }
+        static void TimPhanNhoNhat(int[] arr)
+        {
+            Console.Write("- Phần tử nhỏ nhất trong mảng là: ");
+            int Min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (Min < 0)
+                {
+                    Min = arr[i];
+
+                }
+                if (arr[i] < 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (Min > arr[i])
+                    {
+                        Min = arr[i];
+                    }
+                }
+            }
+            Console.WriteLine($"{Min}");
+        }
+        static void TimPhanLonNhat(int[] arr)
+        {
+            Console.Write("- Phần tử lớn nhất trong mảng là: ");
+            int Max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (Max < arr[i])
+                {
+                    Max = arr[i];
+                }
+            }
+            Console.WriteLine($"{Max}");
+        }
+        static void ViTriSoNTDauTien(int[] arr)
+        {
+            
+            Console.Write("- Vị trí số nguyên tố đầu tiên trong mảng là: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                bool k = KiemTraSoNguyenTo(i);
+                if (k == true)
+                {
+                    Console.WriteLine($"arrA[{i}]");
+                    break;
+                }
+            }
+        }
+        static void TimVTCuoiCung(int[] arr)
+        {
             int x = 0;
             Console.Write("   Nhập phần tử cần xác định: ");
             x = Convert.ToInt32(Console.ReadLine());
             int dem = 0;
             int vTri = 0;
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
-                if (x == g[i])
+                if (x == arr[i])
                 {
                     dem++;
                     vTri = i;
@@ -132,185 +161,128 @@ namespace CHUONG_VII
             }
             if (dem == 0)
             {
-                Console.WriteLine($"   Phần tử {x} không nằm trong mảng A.\n");
+                Console.WriteLine($"- Phần tử {x} không nằm trong mảng A.\n");
             }
             else
             {
-                Console.WriteLine($"   Vị trí cuối cùng của phần tử {x} là: A[{vTri}].\n");
+                Console.WriteLine($"- Vị trí cuối cùng của phần tử {x} là: A[{vTri}].\n");
             }
         }
-        //Hàm H
-        static void vTriSNT(int[] h,int n)
+        static void DemSoHoanThien(int[] arr)
         {
             int dem = 0;
-            for (int i = 0; i < n; i++)
+            foreach (int i in arr)
             {
-                for (int j = 1; j <= h[i]; j++)
-                {
-                    if (h[i] % j == 0)
-                    {
-                        dem++;
-                    }
-                }
-                if (dem == 2)
-                {
-                    Console.WriteLine($"A[{i}]");
-                    break;
-                }
-                dem = 0;
-            }
-        }
-
-        //Hàm I
-        static void pTLNhat(int[] I,int n)
-        {
-            int Max = I[0];
-            for (int i = 1; i < n; i++)
-            {
-                if (Max < I[i])
-                {
-                    Max = I[i];
-                }
-            }
-            Console.WriteLine($"{Max}");
-        }
-
-        //Hàm J
-        static void pTNNhat(int[] J, int n)
-        {
-            int Min = J[0];
-            for (int i = 1; i < n; i++)
-            {
-                if (Min < 0)
-                {
-                    Min = J[i];
-                    
-                }
-                if (J[i] < 0)
-                {
-                    continue;
-                }
-                else
-                {
-                    if (Min > J[i])
-                    {
-                        Min = J[i]; 
-                    }
-                }
-            }
-            Console.WriteLine($"{Min}");
-        }
-
-        //Hàm K
-        static void SORT(int[] A, int[] B, int n)
-        {
-            for (int k = 0; k < n;k ++)
-            {
-                B[k] = A[k];
-            }
-            int i;
-            int j;
-            for (i = 0; i < n - 1; i++)
-            {
-                for (j = n - 1; j > i; j--)
-                {
-                    if (B[j] < B[j - 1])
-                    {
-                        int tmp = B[j];
-                        B[j] = B[j - 1];
-                        B[j - 1] = tmp;
-                    }
-                }
-            }
-            XMang(B, n);
-        }
-        
-        //Hàm L
-        static void ktrsx(int[] A,int n)
-        {
-            int dem = 0;
-            for (int i = 0;i < n; i++)
-            {
-                if (A[i] < A[i + 1])
+                bool k = KiemTraSoHoanThien(i);
+                if (k == true)
                 {
                     dem++;
                 }
             }
-            if (dem == n - 1)
-            {
-                Console.WriteLine("   Mảng A tăng dần.");
-            }
-            else
-            {
-                Console.WriteLine("   Mảng A không tăng dần.");
-            }
+            Console.WriteLine($"- Số số hoàn thiện trog mảng là: {dem}");
         }
-        static void Main(string[] args)
+        static bool KiemTraSoHoanThien(int so)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            int n = 0;
-            int x = 0;
-            int[] A = new int[101];
-            int[] B = new int[101];
-            
-            Console.Write("Nhập vào số nguyên dương n (1->100): ");
-            n = Convert.ToInt32(Console.ReadLine());
-
-            if (n < 1 || n > 100)
+            bool k = false;
+            int dem = 0;
+            for (int i = 1; i < so; i++)
             {
-                Console.WriteLine(" Vui lòng nhập lại!!!");
+                if (so % i == 0)
+                {
+                    dem += i;
+                }
+            }
+            if(dem == so)
+            {
+                k = true;
+            }
+            return k;
+        }
+        static double TrungBinhCong(int[] arrA)
+        {
+            double TrungBinh = 0;
+            int dem = 0;
+            foreach (int i in arrA)
+            {
+                TrungBinh += (double)i;
+                dem++;
+            }
+            return TrungBinh/=(double)dem;
+        }
+        static void XuatSoNguyenTo(int[] arr)
+        {
+            Console.WriteLine("- Các số nguyên tố trong mảng là:");
+            foreach (int i in arr)
+            {
+                bool KiemTra = KiemTraSoNguyenTo(i);
+                if (KiemTra == true)
+                {
+                    Console.Write($"{i}  ");
+                }
+            }
+            Console.WriteLine();
+        }
+        static bool KiemTraSoNguyenTo(int so)
+        {
+            bool k = true;
+            if (so == 1)
+            {
+                k = false;
             }
             else
             {
-                //A
-                Console.WriteLine($"a. Nhập vào số phần tử trong mảng A:");
-                nMang(A, n);
+                for (int i = 2; i <= Math.Sqrt(so); i++)
+                {
+                    if (so % i == 0)
+                    {
+                        k = false;
+                        break;
+                    }
 
-                //B
-                Console.WriteLine("\nb. Tất cả các phần tử trong mảng A là:");
-                XMang(A, n);
-
-                //C
-                Console.Write("\n\nc. Các số chẵn trong mảng A là: ");
-                iSoChan(A, n);
-
-                //D
-                Console.Write("\n\nd. Các số nguyên tố trong mảng A là: ");
-                iSoNT(A, n);
-                
-                //E
-                Console.Write("\n\ne. Trung bình cộng các phần tử trong mảng A là: ");
-                tTB(A, n);
-
-                //F
-                Console.Write("\nf. Số lượng số hoàn thiện trong mảng A là: ");
-                kTraSHoanThien(A,   n);
-
-                //G
-                Console.WriteLine("\ng. Vị trí cuối cùng của phần tử trong mảng A:");
-                tVTCuoiCung(A, n);
-                
-                //H
-                Console.Write("h. Vị trí số nguyên tố đầu tiên trong mảng A là: ");
-                vTriSNT(A, n);
-
-                //I
-                Console.Write("\ni. Phần tử lớn nhất trong mảng A là: ");
-                pTLNhat(A, n);
-
-                //J
-                Console.Write("\nj. Số dương nhỏ nhất trong mảng A là: ");
-                pTNNhat(A, n);
-
-                //K
-                Console.WriteLine("\nk. Mảng A được sắp xếp theo thứ tự tăng dần là:");
-                
-                SORT(A, B, n);
-
-                //L
-                Console.WriteLine("\n\nl. Kiểm tra mảng có thứ tự tăng hay không?");
-                ktrsx(A, n);
+                }
             }
+            return k;
+        }
+        static void XuatSoChan(int[] arrA)
+        {
+            Console.WriteLine("- Các số chẵn trong mảng là:");
+            foreach (int i in arrA)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write($"{i}  ");
+                }
+            }
+            Console.WriteLine();
+        }
+        static void XuatMang(int[] arrA)
+        {
+            foreach (int i in arrA)
+            {
+                Console.Write($"{i}  ");
+            }
+            Console.WriteLine();
+        }
+        static int[] NhapMang(int n)
+        {
+            int[] arrA = new int[n];
+            Console.WriteLine("- Nhập vào các phần tử của mảng:");
+            for (int i = 0; i < arrA.Length; i++)
+            {
+                Console.Write($"arrA[{i}] = ");
+                arrA[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return arrA;
+        }
+        static int NhapSoLuongPhanTu()
+        {
+            int n = 0;
+            do
+            {
+                Console.WriteLine("Nhập vào số lượng phần tử của mảng:");
+                n = int.Parse(Console.ReadLine());
+            } while (n < 0);
+            return n;
         }
     }
 }
